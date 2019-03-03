c65d1e150be296a6b86a55d17058360af7b19e1c
Natasha Henry <natashahenry@Natashas-MacBook-Pro.local>
2019-03-03 15:21:56 -0500

    Added array of questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+$(document).ready() = {
+
+    var questions = [
+        {
+            prompt: "Who are Jon Snow's parents?",
+            choice: ["Ned Stark  and Catelyn Stark", "Rhaegar Targaryen and Lyanna Stark", "Rob Snow and Cersei Lannister", "no one knows"],
+            answer: "1",
+        },
+        {
+            prompt: "How many dire wolf pups were found and given to the Stark children?",
+            choice: ["6", "2", "5", "8"],
+            answer: "0",
+        },
+        {
+            prompt: "How many titles does Daenerys Targaryen hold?",
+            choice: ["6", "2", "5", "7"],
+            answer: "3",
+        },
+        {
+            prompt: "What was the name of Arya Stark's dire wolf?",
+            choice: ["Nymeria", "Ghost", "Lady", "Summer"],
+            answer: "0",
+        },
+        {
+            prompt: "What is the name of Sansa Stark's first husband?",
+            choice: ["Ramsay Bolton", "Joffrey Baratheon", "Tyrion Lannister", "Loras Tyrell"],
+            answer: "2",
+        },
+        {
+            prompt: "What did Arya serve Walder Frey before she killed him?",
+            choice: ["wine", "his sons cooked in a meat pie", "his wife and unborn child in a casserole", "roasted pheasant"],
+            answer: "1",
+        },
+        {
+            prompt: "The massacre that was arranged by Lord Walder Frey to kill King Rob Stark's party which included Rob Stark's wife (and unborn child) and his mother, Catelyn Stark, was later known as?",
+            choice: ["The Purple Wedding", "The Massacre at The Freys", "The Red Wedding", "Death of Hospitality"],
+            answer: "2",
+        },
+    ]
+    var score = 0;
+
+    for(var i = 0; i < questions.length; i++);
+
+
+}
+
